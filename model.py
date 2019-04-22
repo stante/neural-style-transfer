076487f8ee233bc107fd29c6f145fe53bcdca3b5
Alexander Stante <stante@gmail.com>
2019-04-22 09:56:50 +0200

    Add model skeleton

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+
+class NeuralStyleTransfer(nn.Module):
+    def __init__(self):
+        super(NeuralStyleTransfer, self).__init__()
+
+    def forward(self, x):
+        pass
