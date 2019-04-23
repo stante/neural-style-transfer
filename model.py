2a44bd336d6a8e8e00085afb66c38a7037b9344a
Alexander Stante <stante@gmail.com>
2019-04-23 18:56:59 +0200

    Add vgg16 based implementation to model

--- model.py
+++ model.py
@@ -1,9 +1,14 @@
 import torch.nn as nn
+import torchvision.models as models
 
 
 class NeuralStyleTransfer(nn.Module):
     def __init__(self):
         super(NeuralStyleTransfer, self).__init__()
+        self.model = models.vgg16(pretrained=True).features
+
+        for param in self.model.parameters():
+            param.requires_grad_(False)
 
     def forward(self, x):
-        pass
+        return self.model(x)
