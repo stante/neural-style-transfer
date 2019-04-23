1fdbea9e6a461a06624ea6c4989ea1bb82deb773
Alexander Stante <stante@gmail.com>
2019-04-23 21:17:11 +0200

    Add progress bar to training loop

--- neural-style-transfer.py
+++ neural-style-transfer.py
@@ -1,13 +1,16 @@
 from model import NeuralStyleTransfer
 import click
+import torch
+import tqdm
 
 
+@click.command()
 @click.option('--epochs', default=2000, help='Number of epochs')
 def main(epochs):
     model = NeuralStyleTransfer()
 
-    for epoch in range(epochs):
-        pass
+    for epoch in tqdm.tqdm(range(epochs)):
+        features = model.forward(torch.randn(1, 3, 224, 224))
 
 
 if __name__ == '__main__':
