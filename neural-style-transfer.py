ede462db0920958a4c6e3d4086d87e386e7ea460
Alexander Stante <stante@gmail.com>
2019-04-23 21:51:41 +0200

    Add load_image function and cli arguments for style and content image

--- neural-style-transfer.py
+++ neural-style-transfer.py
@@ -2,15 +2,30 @@ from model import NeuralStyleTransfer
 import click
 import torch
 import tqdm
+import os
+from PIL import Image
+import torchvision.transforms as transforms
 
 
 @click.command()
 @click.option('--epochs', default=2000, help='Number of epochs')
-def main(epochs):
+@click.argument('style-image')
+@click.argument('content-image')
+def main(epochs, style_image, content_image):
     model = NeuralStyleTransfer()
+    style_tensor = load_image(os.path.expanduser(style_image))
 
     for epoch in tqdm.tqdm(range(epochs)):
-        features = model.forward(torch.randn(1, 3, 224, 224))
+        features = model.forward(style_tensor)
+
+
+def load_image(path):
+    transform = transforms.ToTensor()
+    image = Image.open(path).convert('RGB')
+    tensor = transform(image)
+    tensor.unsqueeze_(0)
+
+    return tensor
 
 
 if __name__ == '__main__':
