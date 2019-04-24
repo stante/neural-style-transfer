68b9bc9ae93fc9de959603a2f8138928cb05dd42
Alexander Stante <stante@gmail.com>
2019-04-24 21:57:01 +0200

    Change model from vgg16 to vgg19

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torchvision.models as models
 class NeuralStyleTransfer(nn.Module):
     def __init__(self):
         super(NeuralStyleTransfer, self).__init__()
-        self.model = models.vgg16(pretrained=True).features
+        self.model = models.vgg19(pretrained=True).features
 
         for param in self.model.parameters():
             param.requires_grad_(False)
